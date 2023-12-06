224da6b38564fd8dd38affa5110c8d7f0b5e8166
Kateřina Štěpařová <k.steparova@seznam.cz>
2023-12-06 19:34:57 +0100

    doplneni dalsich html slozek podmenu

---
+++ .eleventy.js
@@ -0,0 +1,27 @@
+module.exports = function(eleventyConfig) {
+
+	// Výchozí výstupní složka: _site
+
+	// Zkopírovat images/ do _site/images
+	eleventyConfig.addPassthroughCopy("image");
+    eleventyConfig.addPassthroughCopy("movie");
+
+	// Zkopírovat css/ to _site/css/
+	eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("scss");
+
+  // toto přidej, abys viděla i druhou adresu serveru v lokální síti
+    eleventyConfig.setServerOptions({showAllHosts: true,});
+
+  
+  return {
+    // možné formáty šablon
+    templateFormats: ["njk", "html", "md", "liquid"],
+
+    // jako šablonovací jazyk zvolíme Nunjucks
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  }
+
+};
